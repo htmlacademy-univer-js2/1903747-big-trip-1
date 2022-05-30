f48b2a0ae3ff487992df39d358556475f61820df
ass_dj <yaromanov2002@gmail.com>
2022-05-30 16:40:03 +0500

    flatpickr готов

--- src/utilities/common.js
+++ src/utilities/common.js
@@ -1,6 +1,15 @@
+import dayjs from 'dayjs';
+
 export const upCaseFirst = (str) => {
   if (!str) {
     return str;
   }
   return str[0].toUpperCase() + str.slice(1);
+};
+
+export const humanizeDateInput = (date) => {
+  if (!(date instanceof Date)) {
+    return '';
+  }
+  return dayjs(date).format('dd/mm/YY HH:mm');
 };
